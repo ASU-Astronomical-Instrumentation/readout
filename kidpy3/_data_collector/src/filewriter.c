1f48dc2a9597ce4fd369f881c7d37169a1394299
Cody Roberson <carobers@asu.edu>
2025-08-19 18:22:15 -0700

    Sort of working hdf5 file extension

--- kidpy3/_data_collector/src/filewriter.c
+++ kidpy3/_data_collector/src/filewriter.c
@@ -1,100 +1,143 @@
 #include "data_collector.h"
 
 typedef struct rawdata_t {
-  hid_t file;
-  hid_t datatype;
-  hid_t cparams;
-  int rank;
-  hsize_t dimms[2];
-  hsize_t chunk_dim[2];
-  hsize_t maxdims[2];
-  hid_t dataspace;
+      hid_t file;
+      hid_t datatype;
+      hid_t c_params;
+      int rank;
+      hsize_t dim[2]; //Current Dimensions
+      hsize_t chunk_dim[2];
+      hsize_t max_dim[2]; //Max Allowed Dimension. (By default, this should be 1024,Unlimited)
+      hid_t dataspace;
 
-  hid_t grp_tod;
-  hid_t dset;
+      hid_t grp_tod;
+      hid_t dset;
 } rawdata_t;
 
 #ifndef __BUILD_FOR_LIB__
 
-int load_rawDataFile(const char *filename) {
-  rawdata_t df;
-  df.file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
-  df.dset = H5Dopen(df.file, "adc_i", H5P_DEFAULT);
-  df.datatype = H5Dget_type(df.dset);
-  df.dataspace = H5Dget_space(df.dset);
-  df.rank = H5Sget_simple_extent_dims(df.dataspace, df.dimms, df.maxdims);
+int modify_raw_data_file(const char *filename) {
 
-  printf("Loaded file, DIMMS = (%ld, %ld)", df.dimms[0], df.dimms[1]);
+    // First, we open the file and collect some of its attributes into a struct
+    rawdata_t df;
+    df.file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
+    df.dset = H5Dopen(df.file, "adc_i", H5P_DEFAULT);
+    df.datatype = H5Dget_type(df.dset);
+    df.dataspace = H5Dget_space(df.dset);
+    df.rank = H5Sget_simple_extent_dims(df.dataspace, df.dim, df.max_dim);
 
-  /* H5Dwrite(df.dset, df.datatype, H5S) */
-  H5Dclose(df.dset);
-  H5Sclose(df.dataspace);
-  H5Tclose(df.datatype);
-  return 0;
+    //Check that we have a dimension of 1024 tones by Unlimited samples
+    printf("Loaded file, Current adc_i DIM=(%ld, %ld)\n", df.dim[0], df.dim[1]);
+    if (df.max_dim[0] == 1024) {
+        printf("Check max DIM[0] set to 1024 pass\n");
+    } else {
+        fprintf(stderr, " max DIM[0] set incorrectly to %ld\n", df.max_dim[0]);
+        return -1;
+    }
+    if (df.max_dim[1] == H5S_UNLIMITED) {
+        printf("Check max DIM[1] set to UNLIMITED pass\n");
+    } else {
+        fprintf(stderr, " max DIM[1] set incorrectly to %ld\n", df.max_dim[1]);
+        return -1;
+    }
+    /* A new data file should always have the dimension 1024,488 however that is technically modifiable upstream
+     * of this operation from data_handler.RawDataFile.format()*/
+
+    //Experiment, Lets extend the dataset.
+    df.dim[1] = df.dim[1] + 1;
+    herr_t status = H5Dset_extent(df.dset,  df.dim);
+    if (status < 0) {
+        fprintf(stderr, "Error occurred when resizing the dataset\n");
+    }
+
+    int FakeData[1024];
+    for (int i = 0; i < 1024; i++) {
+        FakeData[i] = i*2;
+    }
+    printf("Define new hyper-slab\n");
+    const hsize_t start[2] = {0, 0};
+    const hsize_t count[2] = {1024, 1};
+    H5Sselect_hyperslab(df.dataspace, H5S_SELECT_SET, start, NULL, count, NULL);
+
+    printf("Setup new dataspace\n");
+    const hid_t mem_space = H5Screate_simple(df.rank, count, NULL);
+
+    printf("Write dataset\n");
+    status = H5Dwrite(df.dset, df.datatype, mem_space, df.dataspace, H5P_DEFAULT, FakeData);
+    if (status < 0) {
+        fprintf(stderr, "Error occurred when writing to the dataset\n");
+        return -1;
+    }
+
+    H5Dclose(df.dset);
+    H5Sclose(df.dataspace);
+    H5Sclose(mem_space);
+    H5Tclose(df.datatype);
+    return 0;
 }
 
 int main(int argc, char **argv) {
-  // Create hdf5 file and populate it with an empty dataset;
-  rawdata_t rawdata;
-  rawdata.dimms[0] = 1024;
-  rawdata.dimms[1] = 488;
-  rawdata.rank = 2;
-  rawdata.chunk_dim[0] = 1024;
-  rawdata.chunk_dim[1] = 1;
-  rawdata.cparams = H5Pcreate(H5P_DATASET_CREATE);
-  rawdata.datatype = H5Tcopy(H5T_NATIVE_INT);
-  rawdata.maxdims[0] = 1024;
-  rawdata.maxdims[1] = H5S_UNLIMITED;
-  H5Tset_order(rawdata.datatype, H5T_ORDER_LE);
+    // Create hdf5 file and populate it with an empty dataset;
+    rawdata_t rawdata;
+    rawdata.dim[0] = 1024;
+    rawdata.dim[1] = 1;
+    rawdata.rank = 2;
+    rawdata.chunk_dim[0] = 1024;
+    rawdata.chunk_dim[1] = 1;
+    rawdata.c_params = H5Pcreate(H5P_DATASET_CREATE);
+    rawdata.datatype = H5Tcopy(H5T_NATIVE_INT);
+    rawdata.max_dim[0] = 1024;
+    rawdata.max_dim[1] = H5S_UNLIMITED;
+    H5Tset_order(rawdata.datatype, H5T_ORDER_LE);
 
-  const char *filename = "my_file.h5";
-  printf("Creating file %s\n", filename);
+    const char *filename = "my_file.h5";
+    printf("Creating file %s\n", filename);
 
-  rawdata.file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-  if (rawdata.file < 0) {
-    fprintf(stderr, "Could not open file %s\n", filename);
-    return -1;
-  }
+    rawdata.file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    if (rawdata.file < 0) {
+          fprintf(stderr, "Could not open file %s\n", filename);
+          return -1;
+    }
 
-  hid_t status = H5Pset_chunk(rawdata.cparams, rawdata.rank, rawdata.chunk_dim);
-  if (status != 0) {
-    fprintf(stderr, "Could not set chunk dataset\n");
-    return -1;
-  }
-  rawdata.dataspace =
-      H5Screate_simple(rawdata.rank, rawdata.dimms, rawdata.maxdims);
-  rawdata.dset =
-      H5Dcreate(rawdata.file, "adc_i", H5T_NATIVE_INT, rawdata.dataspace,
-                H5P_DEFAULT, rawdata.cparams, H5P_DEFAULT);
+    hid_t status = H5Pset_chunk(rawdata.c_params, rawdata.rank, rawdata.chunk_dim);
+    if (status != 0) {
+        fprintf(stderr, "Could not set chunk dataset\n");
+        return -1;
+    }
+    rawdata.dataspace =
+        H5Screate_simple(rawdata.rank, rawdata.dim, rawdata.max_dim);
+    rawdata.dset =
+        H5Dcreate(rawdata.file, "adc_i", H5T_NATIVE_INT, rawdata.dataspace,
+                  H5P_DEFAULT, rawdata.c_params, H5P_DEFAULT);
 
-  status = H5Tclose(rawdata.datatype);
-  if (status < 0) {
-    fprintf(stderr, "Could not close datatype\n");
-    return -1;
-  }
+    status = H5Tclose(rawdata.datatype);
+    if (status < 0) {
+        fprintf(stderr, "Could not close datatype\n");
+        return -1;
+    }
 
-  status = H5Dclose(rawdata.dset);
-  if (status < 0) {
-    fprintf(stderr, "Could not close dataset\n");
-    return -1;
-  }
+    status = H5Dclose(rawdata.dset);
+    if (status < 0) {
+        fprintf(stderr, "Could not close dataset\n");
+        return -1;
+    }
 
-  status = H5Sclose(rawdata.dataspace);
-  if (status < 0) {
-    fprintf(stderr, "Could not close dataspace\n");
-    return -1;
-  }
+    status = H5Sclose(rawdata.dataspace);
+    if (status < 0) {
+        fprintf(stderr, "Could not close dataspace\n");
+        return -1;
+    }
 
-  status = H5Fclose(rawdata.file);
-  if (status < 0) {
-    fprintf(stderr, "Could not close data file\n");
-    return -1;
-  }
+    status = H5Fclose(rawdata.file);
+    if (status < 0) {
+        fprintf(stderr, "Could not close data file\n");
+        return -1;
+    }
 
-  printf("Okay, we made it this far and generated an HDF5 file. Now we can "
-         "load it.\n");
-  load_rawDataFile(filename);
-  return 0;
+    printf("Okay, we made it this far and generated an HDF5 file. Now we can "
+           "load it.\n");
+    modify_raw_data_file(filename);
+    return 0;
 }
 
 #endif
